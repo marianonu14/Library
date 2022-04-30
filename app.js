d9732266187488571053bba9b9db9c891730b148
marianonu14 <marianonunezma@gmail.com>
2022-04-30 15:03:56 -0300

    First Steps With JS

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('#btn');
+const container = document.querySelector('#container');
+let myLibrary = [];
+
+btn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+
+function Book() {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = false
+  }
+  
+function addBookToLibrary() {
+    // do stuff here
+  }
